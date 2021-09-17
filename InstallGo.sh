6ee127de85afea8a9a4e8204c1c97ea515886837
Lyciums <32834542+Traced@users.noreply.github.com>
2021-09-17 15:31:10 +0800

    fix

--- InstallGo.sh
+++ InstallGo.sh
@@ -4,20 +4,21 @@
 wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
 tar -xf go1.17.1.linux-amd64.tar.gz
 
-# Hidden in directory .software, protect against deletion
-mkdir .software
-mv go .software
-mkdir .software/gopackages
+
+rm -rf /home/sandbox/software
+mkdir /home/sandbox/software
+mv go /home/sandbox/software
+mkdir /home/sandbox/software/gopackages
 
 # Written environment variable to the .temp_env files
-touch .temp_env
+touch  .temp_env
 cat > .temp_env <<EOF
 GO111MODULE=auto
-HOME_DIR=/home/sandbox/.software
-GOROOT=$HOME_DIR/go
-GOPATH=$HOME_DIR/gopackages
-PATH=$GOROOT/bin:$PATH
+HOME_DIR=/home/sandbox/software
+GOROOT=\$HOME_DIR/go
+GOPATH=\$HOME_DIR/gopackages
+PATH=\$GOROOT/bin:\$PATH
 EOF
 
 # Let it take effect
-. .temp_env
+source .temp_env
